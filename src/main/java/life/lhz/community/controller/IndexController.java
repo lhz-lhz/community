5952d3f96ac1e5403bb8160407839cd32cce252d
unknown <1442620724@qq.com>
2019-10-30 17:44:38 +0800

    add check cookies

--- src/main/java/life/lhz/community/controller/IndexController.java
+++ src/main/java/life/lhz/community/controller/IndexController.java
@@ -1,14 +1,38 @@
 package life.lhz.community.controller;
 
+
+import life.lhz.community.mapper.UserMapper;
+import life.lhz.community.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
 
 @Controller
 public class IndexController {
-    @GetMapping("/" )
-    public String index() {
-        return "index";
+    @Autowired
+    private UserMapper userMapper;
+
+
+
+    @GetMapping("/")
+    public String index(HttpServletRequest request) {
+        Cookie[] cookies = request.getCookies();
+        for (Cookie cookie : cookies) {
+            if (cookie.getName().equals("token")) {
+                String token = cookie.getValue();
+                User user = userMapper.findByToken(token);
+                if (user != null) {
+                    request.getSession().setAttribute("user", user);
+                }
+                break;
+            }
+        }
+
+            return "index";
+        }
+
+
     }
-
-
-}
